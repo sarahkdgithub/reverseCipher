6574da9a2b6ca3f85bbf2ba86fab1e3e462f594a
sarah berto <sarahberto28@gmail.com>
2020-02-13 21:28:08 +0300

    first commit

---
+++ reverseCipher.py
@@ -0,0 +1,29 @@
+#Reverse Cipher
+
+
+message = 'Be you and do not be a copy of others.'
+translated = ''
+
+
+i = len(message) - 1
+while i >= 0:
+    translated = translated + message[i]
+    i = i-1
+
+print(translated)
+
+
+message = '.srehto fo ypoc a eb ton od dna uoy eB'
+translated = ''
+
+i = len(message) - 1
+while i >= 0:
+    translated = translated + message[i]
+    i = i-1
+
+
+print(translated)
+
+
+
+
